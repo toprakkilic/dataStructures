2534f38f4a7b6a713220dcbe7d3ea660ee0d8bdc
Toprak KILIÇ <toprakkilic23@gmail.com>
2024-10-22 18:32:45 +0300

    Some changes

--- fallHomework_1/src/Main.java
+++ fallHomework_1/src/Main.java
@@ -4,10 +4,11 @@ public class Main {
     public static void main(String[] args) throws Exception {
         //postfix bir ifadeyi hesaplayan kod
 
-
         //eğer gereğinden fazla işlem girersem stack boş hatası alırım
 
         //eğer işlemi eksik girersem sadece olan işlem kadar hesaplama yapar
+
+        //yaptığım son düzenlemelere doğru kodu integer yerine float şekilde çalıştırmaya karar verdim
 
         Scanner sc = new Scanner(System.in);
         String postfix = sc.nextLine();
@@ -18,7 +19,7 @@ public class Main {
       // girilen şeyin bir rakam mı olduğunu kontrol etmem gerekti bunun için parseint keşfettim.
     public static boolean isRakam (String str){
         try {
-            Integer.parseInt(str);
+            Float.parseFloat(str);
             return true;
         } catch (NumberFormatException e) {
             return false;
@@ -26,7 +27,7 @@ public class Main {
     }
 
     //test ederken 0 a bölünmde hata olduğunu gördüm ve hata mesajı ekledim
-    public static int islemYap (String islem, int t, int k) throws Exception {
+    public static float islemYap (String islem, float t, float k) throws Exception {
 
         switch (islem){
             case "+":
@@ -41,28 +42,31 @@ public class Main {
                 }
                 return t / k;
             case "^": //üs alma işlemini yapmadığını fark ettim
-                return (int)Math.pow(t,k);
+                return (float) Math.pow(t,k);
+
+            case "%":
+                return (float) t % k; //artık mod da alıyor
 
                 default:throw new Exception(new Exception(" gecersiz islem"));
         } 
     }
     //burada hesaplamaya çalışıyorum
-    public static int hesapla (String str) throws Exception {
+    public static float hesapla (String str) throws Exception {
 
         //algoritmasını yazmak için postfix ifadeyi anlamak zor oldu
-        GenericStack<Integer> stack = new GenericStack<>(Integer.class, str.length());
+        GenericStack<Float> stack = new GenericStack<>(Float.class, str.length());
         String[] dizi = str.split("_");
 
         for (String haneler : dizi){
             if (isRakam(haneler)){
-                stack.push(Integer.parseInt(haneler));
+                stack.push(Float.parseFloat(haneler));
             }
             else {
 
-                //numaraları yazarken başta ters yazmışkm yanlış cevap veriyordu
-                int n2 = stack.pop();
-                int n1 = stack.pop();
-                int sonuc = islemYap(haneler, n1, n2);
+                //numaraları yazarken başta ters yazmışkm yanlış cevap veriyordu çünkü LIFO olduğunu unuttum
+                float n2 = stack.pop();
+                float n1 = stack.pop();
+                float sonuc = islemYap(haneler, n1, n2);
                 stack.push(sonuc);
             }
         }
